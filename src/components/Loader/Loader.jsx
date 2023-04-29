3387b9cb3be1b92b4b9d256374ecba6d938c21c9
Anna Sibova <anna_sibova@ukr.net>
2023-04-30 01:35:53 +0300

    fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,9 +4,9 @@ import css from './Loader.module.css';
 const Loader = () => {
   return (
     <MutatingDots
-      height="100"
-      width="100"
-      color="#4fa94d"
+      // height="100"
+      // width="100"
+      // color="#4fa94d"
       secondaryColor="#4fa94d"
       radius="12.5"
       ariaLabel="mutating-dots-loading"
